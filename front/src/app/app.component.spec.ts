88c2f2fcb9475cd27ecd2ff9adb26c6647fc2d07
mabdou <abdoumelouk96@gmail.com>
2025-08-28 19:15:12 +0200

    test(front): add basic AppComponent test to improve coverage

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,14 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {JokesService} from "./services/jokes.service";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { JokesService } from './services/jokes.service';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+  let jokesServiceSpy: jasmine.SpyObj<JokesService>;
+
   beforeEach(async () => {
+    jokesServiceSpy = jasmine.createSpyObj('JokesService', [
+      'joke$',
+      'getRandomJoke',
+    ]);
+    jokesServiceSpy.joke$.and.returnValue(
+      of({ id: 1, joke: 'Funny joke', response: 'ok' })
+    );
+
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
-      providers: [JokesService]
+      providers: [{ provide: JokesService, useValue: jokesServiceSpy }],
     }).compileComponents();
   });
 
@@ -16,5 +25,17 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should have a joke$ observable', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.joke$).toBeDefined();
+  });
+
+  it('should call getRandomJoke on init', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges(); // 👈 déclenche ngOnInit()
+    expect(jokesServiceSpy.getRandomJoke).toHaveBeenCalled();
   });
 });
