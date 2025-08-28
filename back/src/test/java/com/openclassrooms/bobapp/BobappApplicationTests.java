d125c3dc99c5b8e55aa35230802576d933561c46
mabdou <abdoumelouk96@gmail.com>
2025-08-28 17:29:33 +0200

    Add code coverage checks and write basic test cases for JokeService

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,13 +1,27 @@
 package com.openclassrooms.bobapp;
 
+import com.openclassrooms.bobapp.model.Joke;
+import com.openclassrooms.bobapp.service.JokeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BobappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private JokeService jokeService;
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+    @Test
+    void jokeServiceShouldReturnJoke() {
+        Joke joke = jokeService.getRandomJoke();
+        assertNotNull(joke);
+    }
 
 }
